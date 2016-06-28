832eb0a156f7d161bb15fe208dba2bd1f8806886
lccodes <luke_camery@brown.edu>
2016-06-28 08:30:25 -0700

    Fully commented

--- edu/brown/exceptions/AgentCreationException.java
+++ edu/brown/exceptions/AgentCreationException.java
@@ -1,10 +1,11 @@
 package brown.exceptions;
 
+/**
+ * Any failure in the construction of an agent
+ * will throw this uncatched exception
+ */
 public class AgentCreationException extends Exception {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = -9089084118116290951L;
 	
 	public AgentCreationException(String message) {
